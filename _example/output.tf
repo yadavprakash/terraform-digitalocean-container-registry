2515f66cd0cad80ca2fe86b0df80484a12bf0117
sohanyadav <sohanyadavdevops@gmail.com>
2024-02-21 15:04:54 +0530

    add output

--- _example/output.tf
+++ _example/output.tf
@@ -1,35 +1,44 @@
 output "id" {
-  value = module.container-registry.id
+  description = "The ID of the container registry."
+  value       = module.container-registry.id
 }
 
 output "name" {
-  value = module.container-registry.name
+  description = "The name of the container registry."
+  value       = module.container-registry.name
 }
 
 output "subscription_tier_slug" {
-  value = module.container-registry.subscription_tier_slug
+  description = "The slug representing the subscription tier of the container registry."
+  value       = module.container-registry.subscription_tier_slug
 }
 
 output "region" {
-  value = module.container-registry.region
+  description = "The region where the container registry is located."
+  value       = module.container-registry.region
 }
 
 output "endpoint" {
-  value = module.container-registry.endpoint
+  description = "The endpoint URL of the container registry."
+  value       = module.container-registry.endpoint
 }
 
 output "server_url" {
-  value = module.container-registry.server_url
+  description = "The URL of the container registry server."
+  value       = module.container-registry.server_url
 }
 
 output "storage_usage_bytes" {
-  value = module.container-registry.storage_usage_bytes
+  description = "The total storage usage in bytes in the container registry."
+  value       = module.container-registry.storage_usage_bytes
 }
 
 output "created_at" {
-  value = module.container-registry.created_at
+  description = "The timestamp when the container registry was created."
+  value       = module.container-registry.created_at
 }
 
 output "expiry_seconds" {
-  value = module.container-registry.expiry_seconds
+  description = "The number of seconds until the container registry expires."
+  value       = module.container-registry.expiry_seconds
 }
